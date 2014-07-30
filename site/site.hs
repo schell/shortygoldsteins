32b16bff21c3169bc3316d9c2aec819e4506e163
Schell Scivally <efsubenovex@gmail.com>
2014-07-29 22:48:35 -0700

    v5

--- site/site.hs
+++ site/site.hs
@@ -12,49 +12,54 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "js/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "pdf/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+--    match (fromList ["about.rst", "contact.markdown"]) $ do
+--        route   $ setExtension "html"
+--        compile $ pandocCompiler
+--            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+--            >>= relativizeUrls
+
+--    match "posts/*" $ do
+--        route $ setExtension "html"
+--        compile $ pandocCompiler
+--            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+--            >>= loadAndApplyTemplate "templates/default.html" postCtx
+--            >>= relativizeUrls
 
     match "pages/*" $ do
         route $ matchRoute "pages/*.html"
               $ customRoute directoryIndex
         compile $ do
-            let aboutCtx =
-                    constField "title" "About" `mappend`
-                    defaultContext
-
             getResourceBody
-                >>= applyAsTemplate aboutCtx
-                >>= loadAndApplyTemplate "templates/default.html" aboutCtx
+                >>= applyAsTemplate defaultContext
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext
                 >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+--    create ["archive.html"] $ do
+--        route idRoute
+--        compile $ do
+--            posts <- recentFirst =<< loadAll "posts/*"
+--            let archiveCtx =
+--                    listField "posts" postCtx (return posts) `mappend`
+--                    constField "title" "Archives"            `mappend`
+--                    defaultContext
+--
+--            makeItem ""
+--                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+--                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+--                >>= relativizeUrls
 
 
     match "index.html" $ do
@@ -63,7 +68,7 @@ main = hakyll $ do
             --posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     --listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Shorty Goldstein's"  `mappend`
+                    --constField "title" "Shorty Goldstein's"  `mappend`
                     defaultContext
 
             getResourceBody
